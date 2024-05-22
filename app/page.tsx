4721e4c48344b8e3850d59d97f10faa8ea66d423
Bruno Prestes <brunoasprestes@gmail.com>
2024-05-22 15:17:21 -0300

    Meta pixel

--- app/page.tsx
+++ app/page.tsx
@@ -8,23 +8,21 @@ import Diferentials from "@/components/Diferentials";
 import Footer from "@/components/Footer";
 import FormCta from "@/components/FormCta";
 import Hero from "@/components/Hero";
+import MetaPixel from "@/components/MetaPixel";
 import Treatment from "@/components/Treatment";
-import Head from "next/head";
 import { GoogleTagManager } from "@next/third-parties/google";
-import CookieNotice from "@/components/CookieNotice";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Dra. Carmila Neiva - Médica Psiquiatra em Macapá',
+  description: "Estou aqui para ouvir suas preocupações e junto com você buscar maneiras de melhorar sua saúde mental e reencontrar sua paz e bem-estar. Vamos juntos definir o melhor plano de tratamento para você, seja ele com terapia, mudanças no estilo de vida, medicamentos ou uma combinação deles.",
+}
 
 export default function Home() {
   return (
     <>
+      <MetaPixel />
       <GoogleTagManager gtmId="GTM-KMRWN8G3" />
-      <Head>
-        <title>Dra. Carmila Neiva - Médica Psiquiatra em Macapá</title>
-        <meta
-          name="description"
-          content="Estou aqui para ouvir suas preocupações e junto com você buscar maneiras de melhorar sua saúde mental e reencontrar sua paz e bem-estar. Vamos juntos definir o melhor plano de tratamento para você, seja ele com terapia, mudanças no estilo de vida, medicamentos ou uma combinação deles."
-          key="desc"
-        />
-      </Head>
       <BtnUp />
       <Hero />
       <Treatment />
